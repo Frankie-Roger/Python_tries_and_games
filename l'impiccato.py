7443fcdadc815ef45531deee6d0d67075899ad59
Frankie <francy.454@gmail.com>
2024-01-09 15:44:40 +0100

    step 1

--- l'impiccato.py
+++ l'impiccato.py
@@ -1,6 +1,10 @@
 import os
+from time import sleep
 
 err = 0
+l_count = 0
+n = 0
+
 matrix_s_m = [
     ['_', '_', '_', '_', '_', '_', '_', '_', '_', ' ', ' ', ' ', ' ', ' '],
     ['|', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
@@ -13,7 +17,54 @@ matrix_s_m = [
     ['|', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
 
 ]
+# parola check
 while True:
+    word = input('Scegli la parola con cui giocare: ').lower().strip()
+    is_ok = True
+    n = False
+    for l in word:
+        l_count += 1
+        try:
+            isinstance(int(l), int)
+            n = True
+        except ValueError:
+            pass
+        if n:
+            print('Non sono amessi numeri nella parola!')
+            sleep(2)
+            os.system('clear')
+            is_ok = False
+            break
+        if l_count > 32:
+            print('Dai mo, non esageriamo! \n\nMassimo 32 lettere.')
+            sleep(2)
+            os.system('clear')
+            is_ok = False
+            break
+        if l == ' ':
+            print('Una sola parola alla volta!\n\nNiente spazi')
+            sleep(2)
+            os.system('clear')
+            is_ok = False
+            break
+    if is_ok:
+        os.system('clear')
+        print('Ottima scelta!\n\nOra si inizia!\n\n')
+        sleep(3)
+        os.system('clear')
+        break
+    else:
+        print('Prova un altra parola.')
+        sleep(2)
+        os.system('clear')
+
+screen_guess = []
+for le in word:
+    screen_guess += '_'
+print(f'{l_count} lettere.\n')
+
+while True:
+    print(screen_guess)
     if err >= 1:
         matrix_s_m[1][8] = '|'
     if err >= 2:
@@ -44,17 +95,15 @@ while True:
         matrix_s_m[7][9] = '\ '
     for row in matrix_s_m:
         print(' '.join(row))
+    guess = input('Inserisci una lettera o la parola: ')
+    if guess == word:
+        print('you win!')
+        break
+    # else:
+    #     for let in guess:
 
-    word = input('scegli la parola con cui giocare: ').lower().strip()
 
-    os.system('clear')
-    break
-    # while True:
-    #     guess = input('Inserisci una lettera o la parola: ')
-    #     for row in matrix_s_m:
-    #         print(' '.join(row))
-    #         break
-    # break
+
 
 
 
